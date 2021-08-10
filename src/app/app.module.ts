0c957a1420231c1af8dc05cd641bb06ed77af245
aditya0701 <adityarawat0701@gmail.com>
2021-08-10 11:28:13 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDescriptionPageComponent } from './product-description-page/product-description-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDescriptionPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-description-page/product-description-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-description-page',
+  templateUrl: './product-description-page.component.html',
+  styleUrls: ['./product-description-page.component.css']
+})
+export class ProductDescriptionPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
