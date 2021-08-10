6e7433e5f59114da68ae4062e422a175622656cb
aditya0701 <adityarawat0701@gmail.com>
2021-08-10 14:25:05 +0530

    Added the change image function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { ProductDescriptionPageComponent } from './product-description-page/prod
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product-description-page/product-description-page.component.ts
+++ src/app/product-description-page/product-description-page.component.ts
@@ -3,13 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-description-page',
   templateUrl: './product-description-page.component.html',
-  styleUrls: ['./product-description-page.component.css']
+  styleUrls: ['./product-description-page.component.css'],
 })
 export class ProductDescriptionPageComponent implements OnInit {
+  images = [
+    { img: '/assets/images/img_1.jpg' },
+    { img: '/assets/images/asusA17.jpg' },
+    { img: '/assets/images/asusA17_2.jpg' },
+    { img: '/assets/images/asusA17_3.jpg' },
+    { img: '/assets/images/asusA17_4.jpg' },
+    { img: '/assets/images/asusA17_5.jpg' },
+    { img: '/assets/images/asusA17_6.jpg' },
+  ];
+  index: number = 0;
+  image: String = this.images[this.index].img;
+  constructor() {}
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  goRight() {
+    console.log('Right button clicked');
+    if (this.index >= this.images.length - 1) {
+      this.index = 0;
+      this.image = this.images[this.index].img;
+      console.log(this.image);
+    } else {
+      this.index++;
+      this.image = this.images[this.index].img;
+      console.log(this.image + ' ' + 'index= ' + this.index);
+    }
   }
-
+  goLeft() {
+    console.log('Left button clicked');
+    if (this.index <= 0) {
+      this.index = this.images.length-1;
+      this.image = this.images[this.index].img;
+      console.log(this.image);
+    } else {
+      this.index--;
+      this.image = this.images[this.index].img;
+      console.log(this.image + ' ' + 'index= ' + this.index);
+    }
+  }
 }
